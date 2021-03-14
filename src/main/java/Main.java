bd0ad576135864cfe1b5a84de6e86a26b9668341
samui <samuila.mihai.g@gmail.com>
2021-03-14 10:52:44 +0200

    final

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("halooo maaaa!");
+        System.out.println(Planets.EUROPA.getEequivalentWeight(20));
+        System.out.println(Planets.WHITE_DWARF.getEequivalentWeight(90));
+
     }
 
 
